fdc6f37cbab0dba8829c7b44a6abce82561a79ed
Laurent <laurent.forthomme@cern.ch>
2016-08-24 17:32:10 +0200

    Introduced the CMS run id<>LHC fill number lookup table (from forthommel/pps-analysis@fc8c0bd) ; implemented the fill-based alignment constants retrieval

--- RecoCTPPS/ProtonProducer/python/protonProducer_cfi.py
+++ RecoCTPPS/ProtonProducer/python/protonProducer_cfi.py
@@ -4,4 +4,6 @@ protons = cms.EDProducer('ProtonProducer',
     protonTag = cms.InputTag('totemRPLocalTrackFitter'),
     useXiInterpolation = cms.bool(True),
     xiInterpolationFile = cms.FileInPath('RecoCTPPS/ProtonProducer/data/ctpps_optics_v1.root'),
+    fillNumLUTFile = cms.FileInPath('RecoCTPPS/ProtonProducer/data/fill_run_lut.dat'),
+    alignmentLUTFile = cms.FileInPath('RecoCTPPS/ProtonProducer/data/alignment_collection.out'),
 )
