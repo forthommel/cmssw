fa093aee3852916ca85392d20dd9d84522b67070
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-05-29 11:38:01 +0200

    First compiling version of the xi particle gun

--- IOMC/ParticleGuns/interface/FlatRandomXiGunProducer.h
+++ IOMC/ParticleGuns/interface/FlatRandomXiGunProducer.h
@@ -6,50 +6,58 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/RandomNumberGenerator.h"
 #include "FWCore/Utilities/interface/StreamID.h"
 
+#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
 #include "CLHEP/Random/RandGauss.h"
 #include "CLHEP/Random/RandFlat.h"
 
-class FlatRandomXiGunProducer : public edm::stream::EDProducer<> {
-  public:
-    explicit FlatRandomXiGunProducer( const edm::ParameterSet& );
-    ~FlatRandomXiGunProducer();
+#include "HepPDT/ParticleDataTable.hh"
 
-    static void fillDescriptions( edm::ConfigurationDescriptions& descriptions );
+namespace edm
+{
+  class FlatRandomXiGunProducer : public edm::stream::EDProducer<> {
+    public:
+      explicit FlatRandomXiGunProducer( const edm::ParameterSet& );
+      ~FlatRandomXiGunProducer();
 
-  private:
-    virtual void beginRun( const edm::Run&, const edm::EventSetup& ) override;
-    virtual void produce( edm::Event&, const edm::EventSetup& ) override;
-    virtual void endRun( const edm::Run&, const edm::EventSetup& ) override;
+      static void fillDescriptions( edm::ConfigurationDescriptions& descriptions );
 
-    CTPPSSimProtonTrack shoot( CLHEP::HepRandomEngine*, double );
+    private:
+      virtual void beginRun( const edm::Run&, const edm::EventSetup& ) override;
+      virtual void produce( edm::Event&, const edm::EventSetup& ) override;
+      virtual void endRun( const edm::Run&, const edm::EventSetup& ) override;
 
-    edm::ESHandle<HepPDT::ParticleDataTable> pdgTable_;
+      HepMC::FourVector shoot( CLHEP::HepRandomEngine*, double );
 
-    edm::ParameterSet partGunParams_;
-    std::vector<int> partIds_;
-    double sqrtS_;
+      edm::ESHandle<HepPDT::ParticleDataTable> pdgTable_;
 
-    edm::ParameterSet beamConditions_;
+      edm::ParameterSet partGunParams_;
+      std::vector<int> partIds_;
+      double sqrtS_;
+      edm::ParameterSet beamConditions_;
 
-    bool simulateVertexX_, simulateVertexY_;
+      double minXi_, maxXi_;
+      double minPhi_, maxPhi_;
 
-    bool simulateScatteringAngleX_, simulateScatteringAngleY_;
-    double thetaPhys_;
+      double thetaPhys_;
+      double vertexSize_;
+      double beamDivergence_;
 
-    bool simulateBeamDivergence_;
-
-    bool simulateXi_;
-    double minXi_;
-    double maxXi_;
-
-    double vertexSize_;
-    double beamDivergence_;
-};
+      bool simulateVertexX_, simulateVertexY_;
+      bool simulateScatteringAngleX_, simulateScatteringAngleY_;
+      bool simulateBeamDivergence_;
+  };
+}
 
 #endif
