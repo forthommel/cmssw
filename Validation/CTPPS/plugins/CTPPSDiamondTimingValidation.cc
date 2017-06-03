e8664cfc44e00e8962847a8d505b5d00ffd58461
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-06-03 15:10:48 +0200

    First attempt to match the z's

--- Validation/CTPPS/plugins/CTPPSDiamondTimingValidation.cc
+++ Validation/CTPPS/plugins/CTPPSDiamondTimingValidation.cc
@@ -29,6 +29,16 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
+#include "DataFormats/CTPPSReco/interface/CTPPSDiamondRecHit.h"
+#include "DataFormats/CTPPSReco/interface/CTPPSDiamondLocalTrack.h"
+
+#include "DataFormats/Common/interface/DetSetVector.h"
+
+#include "CLHEP/Units/PhysicalConstants.h"
+#include "CLHEP/Units/SystemOfUnits.h"
+
 class CTPPSDiamondTimingValidation : public edm::one::EDAnalyzer<edm::one::SharedResources>
 {
   public:
@@ -42,9 +52,15 @@ class CTPPSDiamondTimingValidation : public edm::one::EDAnalyzer<edm::one::Share
     virtual void analyze( const edm::Event&, const edm::EventSetup& ) override;
     virtual void endJob() override;
 
+    edm::EDGetTokenT< edm::View<reco::Vertex> > verticesToken_;
+    edm::EDGetTokenT< edm::DetSetVector<CTPPSDiamondRecHit> > diamRecHitsToken_;
+    edm::EDGetTokenT< edm::DetSetVector<CTPPSDiamondLocalTrack> > diamTracksToken_;
 };
 
-CTPPSDiamondTimingValidation::CTPPSDiamondTimingValidation( const edm::ParameterSet& iConfig )
+CTPPSDiamondTimingValidation::CTPPSDiamondTimingValidation( const edm::ParameterSet& iConfig ) :
+  verticesToken_( consumes< edm::View<reco::Vertex> >( iConfig.getParameter<edm::InputTag>( "verticesTag" ) ) ),
+  diamRecHitsToken_( consumes< edm::DetSetVector<CTPPSDiamondRecHit> >( iConfig.getParameter<edm::InputTag>( "diamondRecHitsTag" ) ) ),
+  diamTracksToken_( consumes< edm::DetSetVector<CTPPSDiamondLocalTrack> >( iConfig.getParameter<edm::InputTag>( "diamondLocalTracksTag" ) ) )
 {
 }
 
@@ -55,6 +71,38 @@ CTPPSDiamondTimingValidation::~CTPPSDiamondTimingValidation()
 void
 CTPPSDiamondTimingValidation::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup )
 {
+  edm::Handle< edm::View<reco::Vertex> > vertices;
+  iEvent.getByToken( verticesToken_, vertices );
+
+  edm::Handle< edm::DetSetVector<CTPPSDiamondRecHit> > rechits;
+  iEvent.getByToken( diamRecHitsToken_, rechits );
+
+  std::vector<CTPPSDiamondRecHit> rechits_45, rechits_56;
+
+  for ( const auto& rechits_ds : *rechits ) {
+    const CTPPSDiamondDetId detid( rechits_ds.detId() );
+    const unsigned short arm = detid.arm();
+    for ( const auto& rechit : rechits_ds ) {
+      if ( arm==0 ) rechits_45.push_back( rechit );
+      if ( arm==1 ) rechits_56.push_back( rechit );
+    }
+  }
+
+  std::vector<double> reco_z;
+  for ( const auto& rh_45 : rechits_45 ) {
+    const double t_45 = rh_45.getT();
+    for ( const auto& rh_56 : rechits_56 ) {
+      const double t_56 = rh_56.getT();
+      reco_z.push_back( ( t_45-t_56 ) / CLHEP::ns * CLHEP::c_light * 0.5 );
+    }
+  }
+
+  for ( const auto& vtx : *vertices ) {
+    const double vtx_z = vtx.z();
+    for ( const auto& z_pp : reco_z ) {
+      std::cout << vtx_z << " >> " << z_pp << std::endl;
+    }
+  }
 }
 
 void 
--- Validation/CTPPS/python/ctppsDiamondTimingValidation_cfi.py
+++ Validation/CTPPS/python/ctppsDiamondTimingValidation_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 ctppsDiamondTimingValidation = cms.EDAnalyzer('CTPPSDiamondTimingValidation',
     diamondRecHitsTag = cms.InputTag('ctppsDiamondRecHits'),
     diamondLocalTracksTag = cms.InputTag('ctppsDiamondLocalTracks'),
-    vertexTag = cms.InputTga('offlinePrimaryVertices'),
+    verticesTag = cms.InputTag('offlinePrimaryVertices'),
 )
