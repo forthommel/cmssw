f74193428b35c4389e10c823cc31db06e9f75703
Laurent Forthomme <laurent.forthomme@cern.ch>
2018-11-22 14:53:19 +0100

    More stealing of https://github.com/MartinKroe/AliRoot

---
+++ EventFilter/DateRawToDigi/src/classes.h
@@ -0,0 +1,11 @@
+#include "EventFilter/DateRawToDigi/interface/AliRawReader.h"
+#include "EventFilter/DateRawToDigi/interface/AliRawReaderDate.h"
+
+namespace EventFilter_DateRawToDigi
+{
+  struct dictionary
+  {
+    //AliRawReader a_rr;
+    AliRawReaderDate a_rrd;
+  };
+}
