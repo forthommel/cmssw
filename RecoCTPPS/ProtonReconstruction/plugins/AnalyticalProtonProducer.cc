0f74044949f3f9c12d87675db2878753ea3d28af
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-01-14 18:50:22 +0100

    Retrieving the optical functions

--- RecoCTPPS/ProtonReconstruction/plugins/AnalyticalProtonProducer.cc
+++ RecoCTPPS/ProtonReconstruction/plugins/AnalyticalProtonProducer.cc
@@ -18,6 +18,9 @@
 #include "DataFormats/ProtonReco/interface/ForwardProton.h"
 #include "DataFormats/ProtonReco/interface/ForwardProtonFwd.h"
 
+#include "CondFormats/DataRecord/interface/CTPPSOpticsRcd.h"
+#include "CondFormats/CTPPSReadoutObjects/interface/LHCOpticalFunctionsSetCollection.h"
+
 class AnalyticalProtonProducer : public edm::stream::EDProducer<> {
 public:
   explicit AnalyticalProtonProducer(const edm::ParameterSet&);
@@ -29,17 +32,26 @@ private:
   void produce(edm::Event&, const edm::EventSetup&) override;
 
   edm::EDGetTokenT<CTPPSLocalTrackLiteCollection> tracksToken_;
+  const std::string opticsTag_;
 };
 
 AnalyticalProtonProducer::AnalyticalProtonProducer(const edm::ParameterSet& iConfig)
-  : tracksToken_(consumes<CTPPSLocalTrackLiteCollection>(iConfig.getParameter<edm::InputTag>("tracksTag"))) {
+    : tracksToken_(consumes<CTPPSLocalTrackLiteCollection>(iConfig.getParameter<edm::InputTag>("tracksTag"))),
+      opticsTag_(iConfig.getParameter<std::string>("opticalFunctionsTag")) {
   produces<reco::ForwardProtonCollection>();
 }
 
 void AnalyticalProtonProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   //--- retrieve the input collection
-  edm::Handle<CTPPSLocalTrackLiteCollection> tracksHandle;
-  iEvent.getByToken(tracksToken_, tracksHandle);
+  edm::Handle<CTPPSLocalTrackLiteCollection> hTracks;
+  iEvent.getByToken(tracksToken_, hTracks);
+  //--- avoid launching the reconstruction if no local tracks in event
+  if (hTracks->empty())
+    return;
+
+  //--- retrieve the conditions
+  edm::ESHandle<LHCOpticalFunctionsSetCollection> hOpticalFunctions;
+  iSetup.get<CTPPSOpticsRcd>().get(opticsTag_, hOpticalFunctions);
 
   //--- book the output
   auto pOut = std::make_unique<reco::ForwardProtonCollection>();
@@ -50,7 +62,8 @@ void AnalyticalProtonProducer::produce(edm::Event& iEvent, const edm::EventSetup
 void AnalyticalProtonProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("tracksTag", edm::InputTag("ctppsLocalTrackLiteProducer"))
-    ->setComment("input local tracks collection");
+      ->setComment("input local tracks collection");
+  desc.add<std::string>("opticalFunctionsTag", "")->setComment("condition database tag for optical functions");
   descriptions.add("ppsProtonsAnalytical", desc);
 }
 
