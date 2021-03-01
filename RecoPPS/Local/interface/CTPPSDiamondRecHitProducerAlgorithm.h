02208fe26bb8fb4d88207893852536c4e669167a
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-03-01 19:15:24 +0100

    DF+Reco: Code-format, TotemT2RecHitProducerAlgorithm now inherits from timing base one

--- RecoPPS/Local/interface/CTPPSDiamondRecHitProducerAlgorithm.h
+++ RecoPPS/Local/interface/CTPPSDiamondRecHitProducerAlgorithm.h
@@ -11,14 +11,15 @@
 
 #include "RecoPPS/Local/interface/TimingRecHitProducerAlgorithm.h"
 
-#include "DataFormats/CTPPSDetId/interface/CTPPSDiamondDetId.h"
 #include "DataFormats/CTPPSDigi/interface/CTPPSDiamondDigi.h"
 #include "DataFormats/CTPPSReco/interface/CTPPSDiamondRecHit.h"
 
-class CTPPSDiamondRecHitProducerAlgorithm : public TimingRecHitProducerAlgorithm<CTPPSDiamondDigi,CTPPSDiamondRecHit> {
+class CTPPSDiamondRecHitProducerAlgorithm : public TimingRecHitProducerAlgorithm<CTPPSDiamondDigi, CTPPSDiamondRecHit> {
 public:
   using TimingRecHitProducerAlgorithm::TimingRecHitProducerAlgorithm;
-  void build(const CTPPSGeometry&, const edm::DetSetVector<CTPPSDiamondDigi>&, edm::DetSetVector<CTPPSDiamondRecHit>&) override;
+  void build(const CTPPSGeometry&,
+             const edm::DetSetVector<CTPPSDiamondDigi>&,
+             edm::DetSetVector<CTPPSDiamondRecHit>&) override;
 
 private:
   static constexpr unsigned short MAX_CHANNEL = 20;
