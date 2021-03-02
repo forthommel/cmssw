3f3b8af64fd9f8d4932b2a41cd51c25c4156dcdd
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-03-02 13:24:08 +0100

    Reco: Inline implementation of base rechits producer template methods

--- RecoPPS/Local/interface/TimingRecHitProducerAlgorithm.h
+++ RecoPPS/Local/interface/TimingRecHitProducerAlgorithm.h
@@ -19,10 +19,15 @@
 template <typename D, typename R>
 class TimingRecHitProducerAlgorithm {
 public:
-  TimingRecHitProducerAlgorithm(const edm::ParameterSet& conf);
+  TimingRecHitProducerAlgorithm(const edm::ParameterSet& iConfig)
+      : ts_to_ns_(iConfig.getParameter<double>("timeSliceNs")),
+        apply_calib_(iConfig.getParameter<bool>("applyCalibration")) {}
   virtual ~TimingRecHitProducerAlgorithm() = default;
 
-  void setCalibration(const PPSTimingCalibration&);
+  void setCalibration(const PPSTimingCalibration& calib) {
+    calib_ = calib;
+    calib_fct_ = std::make_unique<reco::FormulaEvaluator>(calib_.formula());
+  }
   virtual void build(const CTPPSGeometry&, const edm::DetSetVector<D>&, edm::DetSetVector<R>&) = 0;
 
 protected:
