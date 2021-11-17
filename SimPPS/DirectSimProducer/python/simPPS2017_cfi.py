5935c33509803ead61c4194f166b8ca57485b1ec
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-11-17 17:13:08 +0100

    Grouping of profiles by year

--- SimPPS/DirectSimProducer/python/simPPS2017_cfi.py
+++ SimPPS/DirectSimProducer/python/simPPS2017_cfi.py
@@ -1,11 +1,9 @@
 import FWCore.ParameterSet.Config as cms
-
 from CalibPPS.ESProducers.ctppsCompositeESSource_cfi import ctppsCompositeESSource as _esComp
 from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import use_single_infinite_iov_entry, p2017
 from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import ppsAssociationCutsESSource as _esAssCuts
 from Geometry.VeryForwardGeometry.commons_cff import cloneGeometry
-from SimPPS.DirectSimProducer.profile_2017_preTS2_cff import profile_2017_preTS2
-from SimPPS.DirectSimProducer.profile_2017_postTS2_cff import profile_2017_postTS2
+from SimPPS.DirectSimProducer.profiles_2017_cff import profile_2017_preTS2, profile_2017_postTS2
 
 ppsAssociationCutsESSource = _esAssCuts.clone()
 use_single_infinite_iov_entry(ppsAssociationCutsESSource, p2017)
@@ -13,7 +11,7 @@ XMLIdealGeometryESSource_CTPPS, ctppsGeometryESModule = cloneGeometry('Geometry.
 
 ctppsCompositeESSource = _esComp.clone(
     generateEveryNEvents = 100,
-    periods = [profile_2017_postTS2, profile_2017_preTS2],
+    periods = [profile_2017_preTS2, profile_2017_postTS2],
     compactViewTag = ctppsGeometryESModule.compactViewTag,
     isRun2 = ctppsGeometryESModule.isRun2
 )
--- SimPPS/DirectSimProducer/python/simPPS2022_cfi.py
+++ SimPPS/DirectSimProducer/python/simPPS2022_cfi.py
@@ -1,18 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 from CalibPPS.ESProducers.ctppsCompositeESSource_cfi import ctppsCompositeESSource as _esComp
-from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import use_single_infinite_iov_entry, p2021
+from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import use_single_infinite_iov_entry, p2022
 from CalibPPS.ESProducers.ppsAssociationCuts_non_DB_cff import ppsAssociationCutsESSource as _esAssCuts
 from Geometry.VeryForwardGeometry.commons_cff import cloneGeometry
-from SimPPS.DirectSimProducer.profiles_2021_cff import profile_2021_default
+from SimPPS.DirectSimProducer.profiles_2022_cff import profile_2022_default
 
 ppsAssociationCutsESSource = _esAssCuts.clone()
-use_single_infinite_iov_entry(ppsAssociationCutsESSource, p2021)
-XMLIdealGeometryESSource_CTPPS, _ctppsGeometryESModule = cloneGeometry('Geometry.VeryForwardGeometry.geometryRPFromDD_2021_cfi')
+use_single_infinite_iov_entry(ppsAssociationCutsESSource, p2022)
+XMLIdealGeometryESSource_CTPPS, _ctppsGeometryESModule = cloneGeometry('Geometry.VeryForwardGeometry.geometryRPFromDD_2022_cfi')
 # not cloning the ctppsGeometryESModule, as it is replaced by the composite ES source
 
 ctppsCompositeESSource = _esComp.clone(
     generateEveryNEvents = 100,
-    periods = [profile_2021_default],
+    periods = [profile_2022_default],
     # geometry (using 2017 here is OK)
     compactViewTag = _ctppsGeometryESModule.compactViewTag,
     isRun2 = _ctppsGeometryESModule.isRun2
@@ -26,4 +26,4 @@ rpIds = cms.PSet(
     rp_56_F = cms.uint32(123)
 )
 
-#generator.energy = profile_2021_default.ctppsLHCInfo.beamEnergy
+#generator.energy = profile_2022_default.ctppsLHCInfo.beamEnergy
