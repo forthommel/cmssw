d579a6cccbd0da52d7a5968702bfac32be2c380f
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-11-18 10:53:28 +0100

    Updated energy for run3 simulations

--- SimPPS/Configuration/python/directSimPPS_cff.py
+++ SimPPS/Configuration/python/directSimPPS_cff.py
@@ -25,10 +25,14 @@ def _modify2018(process):
 def _modify2021(process):
     print('Process customised for 2021 PPS era')
     process.load('SimPPS.DirectSimProducer.simPPS2021_cfi')
+    if hasattr(process, 'generator'):
+        process.generator.energy = process.profile_2021_default.ctppsLHCInfo.beamEnergy
 
 def _modify2022(process):
     print('Process customised for 2022 PPS era')
     process.load('SimPPS.DirectSimProducer.simPPS2022_cfi')
+    if hasattr(process, 'generator'):
+        process.generator.energy = process.profile_2022_default.ctppsLHCInfo.beamEnergy
 
 modifyConfigurationStandardSequencesFor2016_ = eras.ctpps_2016.makeProcessModifier(_modify2016)
 modifyConfigurationStandardSequencesFor2017_ = eras.ctpps_2017.makeProcessModifier(_modify2017)
