e3a8a50c692d3e412dbf7d3b2ac6532973bd004c
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-11-18 12:05:09 +0100

    Removing ctppsGeometryESModule from run3 recipes (loaded by default by the recoCTPPS sequence container)

--- SimPPS/Configuration/python/directSimPPS_cff.py
+++ SimPPS/Configuration/python/directSimPPS_cff.py
@@ -27,12 +27,18 @@ def _modify2021(process):
     process.load('SimPPS.DirectSimProducer.simPPS2021_cfi')
     if hasattr(process, 'generator'):
         process.generator.energy = process.profile_2021_default.ctppsLHCInfo.beamEnergy
+    if hasattr(process, 'ctppsGeometryESModule'):
+        # replaced by the composite ESSource
+        delattr(process, 'ctppsGeometryESModule')
 
 def _modify2022(process):
     print('Process customised for 2022 PPS era')
     process.load('SimPPS.DirectSimProducer.simPPS2022_cfi')
     if hasattr(process, 'generator'):
         process.generator.energy = process.profile_2022_default.ctppsLHCInfo.beamEnergy
+    if hasattr(process, 'ctppsGeometryESModule'):
+        # replaced by the composite ESSource
+        delattr(process, 'ctppsGeometryESModule')
 
 modifyConfigurationStandardSequencesFor2016_ = eras.ctpps_2016.makeProcessModifier(_modify2016)
 modifyConfigurationStandardSequencesFor2017_ = eras.ctpps_2017.makeProcessModifier(_modify2017)
