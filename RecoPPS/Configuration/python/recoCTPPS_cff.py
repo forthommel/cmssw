4dffc620194e1d2af0c9e0dec128707d38131f33
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-04-28 18:51:47 +0200

    Reco: dropped geometry loading from DB in reco include

--- RecoPPS/Configuration/python/recoCTPPS_cff.py
+++ RecoPPS/Configuration/python/recoCTPPS_cff.py
@@ -9,7 +9,6 @@ from RecoPPS.Local.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLitePro
 
 from RecoPPS.ProtonReconstruction.ctppsProtons_cff import *
 
-from Geometry.VeryForwardGeometry.geometryRPFromDB_cfi import *
 from CalibPPS.ESProducers.ppsTopology_cff import *
 
 recoCTPPSTask = cms.Task(
