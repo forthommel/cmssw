1a2c6a3bee7c68251efea8ebb8843948fa9e1096
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-02-22 01:40:12 +0100

    Small improvements in all parts of emulator + interface
    - more flexibility in Slink header/trailer definition
    - fixed 64b endianness, thanks to joint debugging with yulunmiao
    - safer dump of packed words through a LogDebug
    - also added a trivial emulator mode (first attempt to fill the digis
      with incorrect mapping)
    - added configuration flags to the test config
    - also store unpacked HGCalElectronicsId-indexed digis
    - added capability to steer individual channels in "trivial" mode emulator
    - added steering of characterisation mode, and number of enabled
      channels per eRx
    - made emulator info metadata storage an optional flag

--- EventFilter/HGCalRawToDigi/test/HGCECONDEmulatorTest.cc
+++ EventFilter/HGCalRawToDigi/test/HGCECONDEmulatorTest.cc
@@ -10,42 +10,37 @@
 #include <chrono>
 #include <random>
 
-
 // http://www.cplusplus.com/reference/random/linear_congruential_engine/
 std::minstd_rand0 myrand;
 
-
 //
 HGCalECONDEmulator::HGCROCChannelCollection_t generateHalfChip(bool rocCharMode) {
-
   HGCalECONDEmulator::HGCROCChannelCollection_t chData;
 
-    //two common mode words
-    for(size_t icm=0; icm<2; icm++) {
-      uint16_t cm = myrand() % 1024;
-      HGCROCChannelDataFrameSpec aSample;
-      aSample.fill(true,false,false,0,cm,0,0);
+  //two common mode words
+  for (size_t icm = 0; icm < 2; icm++) {
+    uint16_t cm = myrand() % 1024;
+    HGCROCChannelDataFrameSpec aSample;
+    aSample.fill(true, false, false, 0, cm, 0, 0);
 
-      chData.push_back(aSample);
-    }
-    
-    //36 channels + 1 calib channel
-    for(size_t ich=0; ich<37; ich++) {
-   
-      uint16_t adc = myrand() % 1024;
-      uint16_t adcm1 = myrand() % 1024;
-      uint16_t tot = myrand() % 2048;
-      uint16_t toa = myrand() % 1024;
-      short tc = myrand() % 2;
-      short tp = myrand() % 2;
-      HGCROCChannelDataFrameSpec aSample;
-      aSample.fill(rocCharMode, tc, tp, adcm1, adc, tot, toa);
-      chData.push_back(aSample);
-    }
-   
-    return chData;
+    chData.push_back(aSample);
+  }
+
+  //36 channels + 1 calib channel
+  for (size_t ich = 0; ich < 37; ich++) {
+    uint16_t adc = myrand() % 1024;
+    uint16_t adcm1 = myrand() % 1024;
+    uint16_t tot = myrand() % 2048;
+    uint16_t toa = myrand() % 1024;
+    short tc = myrand() % 2;
+    short tp = myrand() % 2;
+    HGCROCChannelDataFrameSpec aSample;
+    aSample.fill(rocCharMode, tc, tp, adcm1, adc, tot, toa);
+    chData.push_back(aSample);
+  }
+
+  return chData;
 }
-
 
 //
 // run for instance with:
@@ -54,8 +49,7 @@ HGCalECONDEmulator::HGCROCChannelCollection_t generateHalfChip(bool rocCharMode)
 //
 // for a measureble amount of time taken
 
-int main(int argc, char** argv) {
-
+int main(int argc, char **argv) {
   //if passed at command line use new cfi
   std::string url("EventFilter/HGCalRawToDigi/test/HGCECONDEmulatorTests_cfi.py");
   if (argc > 1)
@@ -72,7 +66,8 @@ int main(int argc, char** argv) {
   bool econdPassthrough = cfg.getParameter<bool>("econdPassthrough");
   uint32_t neRx = cfg.getParameter<uint32_t>("neRx");
   int32_t seed = cfg.getParameter<int32_t>("seed");
-  if(seed<0) seed = std::chrono::system_clock::now().time_since_epoch().count();
+  if (seed < 0)
+    seed = std::chrono::system_clock::now().time_since_epoch().count();
 
   std::cout << "Basic performance " << algo << " tests for HGCECONDEmulator" << std::endl
             << "\t + repetitions [int]: " << repetitions << std::endl
@@ -82,16 +77,13 @@ int main(int argc, char** argv) {
   myrand.seed(seed);
 
   // do the trials: time/performance test and exploit randomisation to check
-  for (uint32_t u=0; u < repetitions; u++) {
-
+  for (uint32_t u = 0; u < repetitions; u++) {
     //generate eRx data
     HGCalECONDEmulator::ERxCollection_t erxData;
-    for(uint32_t n=0; n < neRx; n++) {
-      erxData.push_back( generateHalfChip(rocCharMode) );
+    for (uint32_t n = 0; n < neRx; n++) {
+      erxData.push_back(generateHalfChip(rocCharMode));
     }
-    
   }
-
 
   return 0;
 }
