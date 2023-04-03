a0a08bced742089eeb2a686c7123c3fc03c5efa6
Pedro <psilva@cern.ch>
2023-04-03 12:04:16 +0200

    adapting to latest TB format

--- EventFilter/HGCalRawToDigi/interface/Emulator.h
+++ EventFilter/HGCalRawToDigi/interface/Emulator.h
@@ -7,45 +7,55 @@
 #include "EventFilter/HGCalRawToDigi/interface/SlinkTypes.h"
 
 namespace hgcal::econd {
-  /// Pure virtual base class for a ECON-D event emulator implementation
+  /**
+     @class Emulator
+     @short Pure virtual base class for a ECON-D event emulator implementation
+  */
   class Emulator {
   public:
     explicit Emulator(const EmulatorParameters& params) : params_(params) {}
     virtual ~Emulator() = default;
-
+    
     /// Fetch the next ECON-D event
-    virtual ECONDEvent next() = 0;
+    virtual ECONDInput next() = 0;
 
   protected:
     const EmulatorParameters params_;
   };
 
-  /// An empty ECON-D payloads emulator
+  /**
+     @class EmptyEmulator
+     @short an empty ECON-D payloads emulator
+  */
   class EmptyEmulator : public Emulator {
   public:
     using Emulator::Emulator;
 
-    ECONDEvent next() override {
+    ECONDInput next() override {
       EventId evt_id{event_id_++, bx_id_++, orbit_id_++};
-      ERxData dummy_data{.adc = {}, .adcm = {}, .toa = {}, .tot = {}, .tctp = {}, .cm0 = 0, .cm1 = 0};
-      ERxEvent empty_evt = {{ERx_t{0 /*chip*/, 0 /*half*/}, dummy_data}};  // map<ERx_t, ERxData>
-      return ECONDEvent{evt_id, empty_evt};
+      ERxData dummy_data{.cm0 = 0, .cm1 = 0, .tctp = {}, .adc = {}, .adcm = {}, .toa = {}, .tot = {}, .meta = {} };
+      ERxInput empty_evt = {{ERxId_t{0 /*chip*/, 0 /*half*/}, dummy_data}};  // map<ERx_t, ERxData>
+      return ECONDInput{evt_id,empty_evt};
     }
 
   private:
     uint32_t event_id_{1}, bx_id_{2}, orbit_id_{3};
   };
 
-  /// A "trivial" ECON-D emulator emulating non-empty ECON-D events
+  /**
+     @class TrivialEmulator
+     @short A "trivial" ECON-D emulator emulating non-empty ECON-D events
+  */
   class TrivialEmulator : public Emulator {
   public:
     using Emulator::Emulator;
 
-    ECONDEvent next() override;
+    ECONDInput next() override;
 
   private:
     uint32_t event_id_{1}, bx_id_{2}, orbit_id_{3};
   };
+
 }  // namespace hgcal::econd
 
 #endif
